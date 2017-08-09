1ab59209f9dfe68d83cdd9346aa772e176799860
prafullpurwar02 <prafullpurwar@qainfotech.com>
2017-08-09 09:55:13 -0700

    Changes for image

--- tests/mock/data/systemList.json.js
+++ tests/mock/data/systemList.json.js
@@ -1,13 +1,15 @@
+import mapBackground1 from '../../../src/images/Auto_pallet1.png'
+
+
 export const systemList = [
   {
     'systemName': 'Machine Schematic - 01',
     'systemLabel': 'Selection = 01',
     'createdDate': 1499470701959,
     'disabled': false,
-    'systemImage': 'https://encrypted-tbn0.gstatic.com/images?' +
-    'q=tbn:ANd9GcTQehDUC9hh3QRSmuMdZuTpi7Q3s0TPNsXCKK7tJwSUiAKhNBI54A',
+    'systemImage': mapBackground1,
     'systemSensorLocation': [
-      {'x': 35, 'y': 15, state: 1},{'x': 85, 'y': 91,state: 1}, {'x': 250, 'y': 63,state: 0}
+      {'x': 265, 'y': 193, state: 1},{'x': 335, 'y': 42,state: 1}, {'x': 150, 'y': 190,state: 0}
     ],
     'systemSensorCount': '03'
   },
@@ -16,10 +18,9 @@ export const systemList = [
     'systemLabel': 'Selection = 02',
     'createdDate': 1499470701984,
     'disabled': false,
-    'systemImage': 'https://encrypted-tbn0.gstatic.com/images?' +
-    'q=tbn:ANd9GcTQehDUC9hh3QRSmuMdZuTpi7Q3s0TPNsXCKK7tJwSUiAKhNBI54A',
+    'systemImage': mapBackground1,
     'systemSensorLocation': [
-      {'x': 85, 'y': 91, state: 1}, {'x': 250, 'y': 63, state: 1}, {'x': 538, 'y': 250, state: 1}, {'x': 510, 'y': '210', state: 1}, {'x': 565, 'y': '210', state: 1}
+        {'x': 265, 'y': 193, state: 1},{'x': 335, 'y': 42,state: 1}, {'x': 150, 'y': 190,state: 0},  {'x': 480, 'y': 150, state: 1}, {'x': 350, 'y': 345, state: 1}
     ],
     'systemSensorCount': '05'
   },
@@ -28,10 +29,9 @@ export const systemList = [
     'systemLabel': 'Selection = 03',
     'createdDate': 1499470702004,
     'disabled': false,
-    'systemImage': 'https://encrypted-tbn0.gstatic.com/images?' +
-    'q=tbn:ANd9GcTQehDUC9hh3QRSmuMdZuTpi7Q3s0TPNsXCKK7tJwSUiAKhNBI54A',
+    'systemImage': mapBackground1,
     'systemSensorLocation': [
-      {'x': 85, 'y': 91, state: 0},{'x': 265, 'y': 91, state: 1},{'x': 355, 'y': 91, state: 0}, {'x': 185, 'y': 91, state: 1}, {'x': 500, 'y': 90, state: 1}, {'x': 564, 'y': 305, state: 1},{'x': 514, 'y': 305, state: 1}
+        {'x': 265, 'y': 193, state: 1},{'x': 335, 'y': 42,state: 1}, {'x': 150, 'y': 190,state: 0}, {'x': 480, 'y': 150, state: 1}, {'x': 300, 'y': 90, state: 1}, {'x': 350, 'y': 345, state: 1},{'x': 514, 'y': 305, state: 1}
     ],
     'systemSensorCount': '07'
   }
